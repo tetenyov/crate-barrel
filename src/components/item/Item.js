e423897cc4ce33972b9a5e146fb21fb02cb5facb
Vovo <tetenev@yandex.ru>
2020-11-17 20:52:58 +0300

    add styles for current-item page

--- src/components/item/Item.js
+++ src/components/item/Item.js
@@ -24,17 +24,27 @@ function Item() {
   return (
     <article className='current-item'>
       <div className='current-item__wrapper-upper'>
-        <h3>{ currentItem.name }</h3>
-        { currentItem.exclusive && <p>Crate and Barrel Exclusive</p> }
+        <div className=''>
+          <h3 className='current-item__heading'>{ currentItem.name }</h3>
+          { currentItem.exclusive && <p className='current-item__exclusive'>Crate and Barrel Exclusive</p> }
+        </div>
+        <div className=''>
+          <p>Rub <span>{currentItem.price}</span></p>
+          <p>Sku: <span>{currentItem.sku}</span></p>
+          <p>
+           <span>0</span>
+           <button type='button'>Add to cart</button>
+          </p>
+        </div>
       </div>
       <div className='current-item__wrapper-lower'>
         <section className='current-item__images'>
-          <h4 className='current-item__images-heading'>Images of the item</h4>
+          <h4 className='visually-hidden'>Images of the item</h4>
           <img src={currentItem.img[0]} />
           <PreviewsList images={currentItem.img}/>
         </section>
         <section className='current-item__details'>
-          <h4 className='current-item__details-heading'>Details of the item</h4> 
+          <h4 className='visually-hidden'>Details of the item</h4> 
           <div>
             <h5>Ship</h5>
             <p>{currentItem.ship}</p>
